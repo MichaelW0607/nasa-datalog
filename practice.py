1ed84b02163d4ed470dcf986b2b7dcdcc76071c3
Michael Williams <113442619+MichaelW0607@users.noreply.github.com>
2022-12-27 15:35:58 -0500

    Update practice.py

--- practice.py
+++ practice.py
@@ -12,4 +12,16 @@ plt.show()
 a = [0, -50, 25, 67, -223]
 b = [0, 5, 7, 5, 9]
 plt.plot(a, b)
+plt.show()
+
+plt.axis([-50, 25, 5, 9])
+plt.plot(a, b)
+plt.show()
+
+plt.title(“My Graph”)
+plt.xlabel(“Array A”)
+plt.ylabel(“Array B”)
+plt.plot(a, b)
+plt.show()
+
 
